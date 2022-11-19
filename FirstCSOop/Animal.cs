957a5157ffe1259ba867a1f11740e603d7cdc705
mredelis <edelitamr@gmail.com>
2022-11-19 09:40:40 -0500

    new project

---
+++ FirstCSOop/Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCSOop
+{
+    internal class Animal
+    {
+        public void move() 
+        {
+            Console.WriteLine("Animal is moving");
+        }
+
+        public void sleep()
+        {
+            Console.WriteLine("Zzzz");
+        }
+
+        public void eat() 
+        {
+            Console.WriteLine("Animal is eating");
+        }
+    }
+}
---
+++ FirstCSOop/IUFO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCSOop
+{
+    internal interface IUFO
+    {
+        public void fly();
+        public void goToHyperspace();
+        public void land();
+    }
+}

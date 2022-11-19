fad4d93fab91d8c736a0c386a1e81ad5985db5bd
D <D'@DESKTOP-E6BTPFM>
2022-11-19 09:27:15 -0500

    Add README.md.

---
+++ FirstCSOop/Cat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCSOop
+{
+    internal class Cat : Animal
+    {
+        private string name = "Simba";
+        public Cat(string name)
+        {
+            this.name = name;
+            Console.WriteLine("I'm a cat and my name is " + name);
+        }
+        public void sing()
+        {
+            Console.WriteLine("Cats meow");
+        }
+        public void wash()
+        {
+            Console.WriteLine("Cats DON'T enjoy bathing daily");
+        }
+        public void turnAround()
+        {
+            Console.WriteLine("Cats are turning RIGHT");
+        }
+    }
+}
---
+++ FirstCSOop/LargeCraft.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCSOop
+{
+    internal class LargeCraft : IUFO
+    {
+        public void fly()
+        {
+            Console.WriteLine("Large craft can fly around the globe");
+        }
+        public void goToHyperspace()
+        {
+            Console.WriteLine("Large craft can't go to hyperspace");
+        }
+        public void land()
+        {
+            Console.WriteLine("Large craft, of course I can land");
+        }
+    }
+}

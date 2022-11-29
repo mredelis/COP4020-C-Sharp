7a9dc74067f5f138b1b323fd0c9bfd719a05e5bd
mredelis <edelitamr@gmail.com>
2022-11-29 14:17:46 -0500

    as2

---
+++ SecondCSOop/Problem1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondCSOop
+{
+    class Problem1
+    {
+        public static IEnumerable<int> myFilter(IEnumerable<int> input)
+        {
+            IEnumerable<int> result;
+            result = input.Where(x => x % 5 == 0).Where(x => x < 50).Select(x => x * x * x).Where(x => x % 2 != 0);
+
+            return result;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random(5);
+            var listForProblem = Enumerable.Range(1, 100).Select(i => rnd.Next() % 101);
+            //var listForProblem = Enumerable.Range(1, 10);
+            var answer = Problem1.myFilter(listForProblem);
+            foreach (int i in answer)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}

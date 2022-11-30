85445d2ded61c704e34a5c55be76ac0e735fbbd2
Edelis <edelitamr@gmail.com>
2022-11-30 09:23:37 -0500

    Problem1

---
+++ Problem1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Problem1
+{
+  class Problem1
+  {
+    public static IEnumerable<int> myFilter(IEnumerable<int> input)
+    {
+      IEnumerable<int> result;
+      result = input.Where(x => x % 5 == 0).Where(x => x < 50).Select(x => x * x * x).Where(x => x % 2 != 0);
+
+      return result;
+    }
+
+  }
+
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Random rnd = new Random(5);
+      var listForProblem = Enumerable.Range(1, 100).Select(i => rnd.Next() % 101);
+      //var listForProblem = Enumerable.Range(1, 10);
+      var answer = Problem1.myFilter(listForProblem);
+      foreach (int i in answer)
+      {
+        Console.WriteLine(i);
+      }
+    }
+  }
+}

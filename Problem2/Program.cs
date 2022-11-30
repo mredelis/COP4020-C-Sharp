9c4bbabde0b576e2c06d2f161eaee81a2e281794
Edelis <edelitamr@gmail.com>
2022-11-30 13:10:07 -0500

    Problem2

--- Problem2/Program.cs
+++ Problem2/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Problem2
+{
+  class Problem2
+  {
+    public static IEnumerable<int> myFilter(IEnumerable<int> input)
+    {
+      IEnumerable<int> result;
+      result = input.Where(x => x % 6 == 0).Where(x => x > 42).Select(x => x * x).Where(x => x % 2 == 0);
+
+      return result;
+    }
+
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Random rnd = new Random();
+      var listForProblem = Enumerable.Range(1, 100).Select(i => rnd.Next() % 101);
+      var answer = Problem2.myFilter(listForProblem);
+
+      foreach (int i in answer)
+      {
+        Console.WriteLine(i);
+      }
+    }
+  }
+}

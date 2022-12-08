e01eb86d2b5b2994dbf35f04a2c3ed0cf1742700
Edelis <edelitamr@gmail.com>
2022-12-08 18:30:24 -0500

    problem 1

--- Test4/Problem1/Program.cs
+++ Test4/Problem1/Program.cs
@@ -5,38 +5,36 @@
     private int admno;
     private string? sname;
     private float eng, math, science;
-    private float total = 0;
+    private float total;
     private float ctotal()
     {
-      float marks = (eng + math + science);
-      total = marks;
-      return total;
+      return this.eng + this.math + this.science;
     }
 
     public void Takedata()
     {
       Console.WriteLine("Enter admission number: ");
-      admno = Convert.ToInt32(Console.ReadLine());
+      this.admno = Convert.ToInt32(Console.ReadLine());
 
       Console.WriteLine("Enter student name: ");
-      sname = Console.ReadLine();
+      this.sname = Console.ReadLine();
 
       Console.WriteLine("Enter english grade: ");
-      eng = (float)Convert.ToDouble(Console.ReadLine());
+      this.eng = (float)Convert.ToDouble(Console.ReadLine());
 
       Console.WriteLine("Enter math grade: ");
-      math = (float)Convert.ToDouble(Console.ReadLine());
+      this.math = (float)Convert.ToDouble(Console.ReadLine());
 
       Console.WriteLine("Enter science grade: ");
-      science = (float)Convert.ToDouble(Console.ReadLine());
+      this.science = (float)Convert.ToDouble(Console.ReadLine());
 
-      total = ctotal();
+      this.total = ctotal();
     }
 
     public void Showdata()
     {
       Console.WriteLine("You have entered ");
-      Console.WriteLine("Admission number: {0}\nName: {1}\nEnglish: {2}\nMath: {3}\nScience: {4}\nTotal: {4}", admno, sname, eng, math, science, total);
+      Console.WriteLine("Admission number: {0}\nName: {1}\nEnglish: {2}\nMath: {3}\nScience: {4}\nTotal: {5}", this.admno, this.sname, this.eng, this.math, this.science, this.total);
     }
   }
 

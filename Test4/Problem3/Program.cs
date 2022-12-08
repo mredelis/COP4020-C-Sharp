6b28d5e745ff52ca90c7b0c77feeab949a149a7b
Edelis <edelitamr@gmail.com>
2022-12-08 14:02:24 -0500

    Problem 3 done

---
+++ Test4/Problem3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Problem3 // Note: actual namespace depends on the project name.
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int count = 0;
+
+      if (File.Exists("STORY.TXT"))
+      {
+        // read content line by line
+        string[] lines = File.ReadAllLines("STORY.TXT");
+        foreach (string line in lines)
+        {
+          if (line[0] != 'A')
+          {
+            count++;
+          }
+        }
+        Console.WriteLine("Number of lines not starting with A are: " + count);
+      }
+    }
+  }
+}

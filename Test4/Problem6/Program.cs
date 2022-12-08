ad4c7e0535af0f8b0b5fe7dd1eb2b3aebe68e6c6
Edelis <edelitamr@gmail.com>
2022-12-08 17:17:46 -0500

    Problem 6 finished

--- Test4/Problem6/Program.cs
+++ Test4/Problem6/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Problem6
+{
+  class Problem6
+  {
+    public static IEnumerable<int> findOdds(IEnumerable<int> input3)
+    {
+      IEnumerable<int> res;
+      res = input3.Select(x => x * x).Where(x => x % 2 != 0);
+      return res;
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int[] numbers3 = { -5, -6, -7, -8, 47, 50, 60, 1000, 2000, 3000 };
+      List<int> lst3 = numbers3.OfType<int>().ToList();
+
+      var answer = Problem6.findOdds(lst3);
+      foreach (int i in answer)
+      {
+        Console.WriteLine(i);
+      }
+    }
+  }
+}

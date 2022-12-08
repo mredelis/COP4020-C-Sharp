861d9b628ae4cd72282aa97c169b5590eb7772b5
Edelis <edelitamr@gmail.com>
2022-12-08 13:35:35 -0500

    problem2

---
+++ Test4/Problem1/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Problem1
+{
+  class Student
+  {
+    private int admno;
+    private string? sname;
+    private float eng, math, science;
+    private float total = 0;
+    private float ctotal()
+    {
+      float marks = (eng + math + science);
+      total = marks;
+      return total;
+    }
+
+    public void Takedata()
+    {
+      Console.WriteLine("Enter admission number: ");
+      admno = Convert.ToInt32(Console.ReadLine());
+
+      Console.WriteLine("Enter student name: ");
+      sname = Console.ReadLine();
+
+      Console.WriteLine("Enter english grade: ");
+      eng = (float)Convert.ToDouble(Console.ReadLine());
+
+      Console.WriteLine("Enter math grade: ");
+      math = (float)Convert.ToDouble(Console.ReadLine());
+
+      Console.WriteLine("Enter science grade: ");
+      science = (float)Convert.ToDouble(Console.ReadLine());
+
+      total = ctotal();
+    }
+
+    public void Showdata()
+    {
+      Console.WriteLine("You have entered ");
+      Console.WriteLine("Admission number: {0}\nName: {1}\nEnglish: {2}\nMath: {3}\nScience: {4}\nTotal: {4}", admno, sname, eng, math, science, total);
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Student student = new Student();
+      student.Takedata();
+      student.Showdata();
+    }
+  }
+}
---
+++ Test4/Problem2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Problem2
+{
+  class Batsman
+  {
+    private int bcode;
+    private string? bname;
+    private int innings, notout, runs;
+    private float batavg;
+    private void calcavg()
+    {
+      batavg = runs / (innings + notout);
+    }
+
+    public void readdata()
+    {
+      Console.WriteLine("Enter batsman code: ");
+      bcode = Convert.ToInt32(Console.ReadLine());
+
+      Console.WriteLine("Enter batsman name: ");
+      bname = Console.ReadLine();
+
+      Console.WriteLine("Enter innings: ");
+      innings = Convert.ToInt32(Console.ReadLine());
+
+      Console.WriteLine("Enter notout: ");
+      notout = Convert.ToInt32(Console.ReadLine());
+
+      Console.WriteLine("Enter runs: ");
+      runs = Convert.ToInt32(Console.ReadLine());
+
+      calcavg();
+    }
+
+    public void displaydata()
+    {
+      Console.WriteLine("You have entered ");
+      Console.WriteLine("Batsman codde: {0}\nBatsman name: {1}\nInnings: {2}\nNotout: {3}\nRuns: {4}\nBating avarage: {4}", bcode, bname, innings, notout, runs, batavg);
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Batsman batsman = new Batsman();
+      batsman.readdata();
+      batsman.displaydata();
+    }
+  }
+}
